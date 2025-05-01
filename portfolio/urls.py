2232981711f57edf847c6aea316f9b987490014e
R <rzq272@gmail.com>
2025-05-01 19:03:24 +0800

    Add temp test url

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import NameView
+from .views import NameView, ProfileView
 
 urlpatterns = [
     path("", NameView.as_view(), name="index"),
+    path("test/", ProfileView.as_view(), name="profile"),
 ]
