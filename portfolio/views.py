3390513c43a16ed28f1f5a530e38e5eebf47bb5b
R <rzq272@gmail.com>
2025-05-01 17:27:15 +0800

    Implement ProfileView to display user profile, experiences, and skills

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,8 +1,22 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from .models import Experience, Profile, SkillCategory
 
 # Create your views here.
 
 
 class NameView(TemplateView):
     template_name = "portfolio/landing.html"
+
+class ProfileView(TemplateView):
+    template_name = "portfolio/profile.html"
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        
+        profile = Profile.objects.first()
+        
+        context['profile'] = profile
+        context['experiences'] = Experience.objects.filter(profile=profile)
+        context['skills'] = SkillCategory.objects.all()
+        return context
