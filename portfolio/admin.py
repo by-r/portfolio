36e52d552248a7fca0ce031296e4d245a2e56b09
R <rzq272@gmail.com>
2025-05-01 16:12:58 +0800

    Register Portfolio model in the Django admin

--- portfolio/admin.py
+++ portfolio/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
-from .models import Experience
-
-# Register your models here.
+from .models import Experience, Portfolio
 
 admin.site.register(Experience)
+admin.site.register(Portfolio)
