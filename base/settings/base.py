7f8c92f0f89b820fc9ed0245640e0ddf31b79f87
R <rzq272@gmail.com>
2025-05-01 16:11:49 +0800

    Update BASE_DIR path in Django settings for correct directory structure

--- base/settings/base.py
+++ base/settings/base.py
@@ -14,7 +14,7 @@ from pathlib import Path
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 
 # Quick-start development settings - unsuitable for production
@@ -124,5 +124,3 @@ STATIC_ROOT = os.path.join(BASE_DIR, "static")
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-
-AUTH_USER_MODEL = "portfolio.ResumeUser"
