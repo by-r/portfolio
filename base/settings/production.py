4e92c13a1eb9f15b7aa39162d42877bfb29f7afb
R <rzq272@gmail.com>
2025-05-04 15:28:41 +0800

    Update production settings: enhance secret key retrieval and enable DEBUG mode from environment variable

--- base/settings/production.py
+++ base/settings/production.py
@@ -6,7 +6,7 @@ from base.settings import get_secret
 from django.core.management.utils import get_random_secret_key
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = get_secret("SECRET_KEY")
-DEBUG = False
+SECRET_KEY = get_secret("SECRET_KEY", get_random_secret_key())
+DEBUG = bool(os.environ.get("DEBUG", False))
 
 ALLOWED_HOSTS = ["*"]
